61050db3d5c43673408c1b1dfb2f9c3c8a4dd6c3
gitdojoaocastro <joaovictor0397@gmail.com>
2024-05-22 01:55:21 -0300

    teste branch4

---
+++ exercicio01 - Copia.pas
@@ -0,0 +1,38 @@
+{
+   exercicio01.pas
+   
+   Copyright 2018 bahge <bahge@ultrabook>
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
+   
+   
+}
+
+
+program exercicio01;
+
+uses crt;
+var 
+	numero, antecessor : integer;
+
+BEGIN
+	writeln('Digite um número inteiro: ');
+	readln(numero);
+	antecessor := numero - 1;
+	writeln('O antecessor do número: ', numero ,' é ', antecessor);
+	
+END.
+
